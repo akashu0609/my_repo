9e1813145a0edcd5c4647c351babc95d6f6b473d
Akash <akashu0609@gmail.com>
2025-08-22 22:54:50 +0530

    this is my first commit

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+// This is a simple Java program
+public class Hello {
+    public static void main(String[] args) {
+        // Prints "Hello, This is my first code in java!" to the console
+        System.out.println("Hello, his is my first code in java!");
+    }
+}
